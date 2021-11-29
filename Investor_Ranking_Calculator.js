4eae2a8938e07e47e0633832d94a52c3459bf85f
trilltown <phil.awich@gmail.com>
2021-11-28 21:11:36 -0500

    Update totalAssets function

--- Investor_Ranking_Calculator.js
+++ Investor_Ranking_Calculator.js
@@ -3,7 +3,18 @@
 const readline = require('readline-sync');
 
 function totalAssets() {
-    
+    let assetIdentified = parseInt(readline.question('How many assets does this investor have?'));
+    let totalValueOfAssets = 0;
+    let assetsArray = [];
 
-
+    for(let assetCounted = 1; assetCounted<= assetIdentified; assetCounted++) {
+        let individualAssetValue = parseFloat(readline.question('What is the value of asset ' + assetCounted + '?'));
+        assetsArray.push(individualAssetValue)
+        let sum = 0;
+        for(let i= 0; i< assetsArray.length; i++) {
+            totalValueOfAssets = sum += assetsArray[i];
+        }
+    }
+    console.log(totalValueOfAssets)
+    return totalValueOfAssets;
 }
